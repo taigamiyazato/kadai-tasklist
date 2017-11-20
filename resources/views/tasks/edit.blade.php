f69500f3ef9f3c79e2924bfbd406bdd5958e3076
taigamiyazato <gonglitaifu@gmail.com>
2017-11-20 10:51:50 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,9 +2,12 @@
 
 @section('content')
 
-    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
-    
+     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('title', 'タイトル:') !!}
+        {!! Form::text('title') !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
-    
-    <p>{{ $task->content }}</p>
+
+    <p>タイトル: {{ $task->title }}</p>
+    <p>メッセージ: {{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
-    
+
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
     {!! Form::close() !!}
